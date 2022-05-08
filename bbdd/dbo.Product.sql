b2a046d8f99ac115ba17c9c2102caef86b5eeef7
Begoña Diaz <labastidafdez@gmail.com>
2022-05-08 17:52:45 +0200

    base.

---
+++ bbdd/dbo.Product.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [Summary]    NVARCHAR (50) NULL,
+    [Icon]       NTEXT         NULL,
+    [Banner]     NTEXT         NULL,
+    [Trailer]    NVARCHAR (50) NULL,
+    [Price]      FLOAT (53)    NULL,
+    [Publishing] DATETIME2 (7) NULL DEFAULT (getdate()),
+    [Size]       FLOAT (53)    NULL,
+    [Developer_id] INT NULL, 
+    [Editor_id] INT NULL, 
+    [Type] TINYINT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
